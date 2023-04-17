f8f9105eba201bafc0e710f7ff6dd2bd3009a356
andreysulymka <andreysulymka5@gmail.com>
2023-04-17 20:39:51 +0300

    add changes

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Item, Button } from "./List.styled";
 import PropTypes from 'prop-types';
+import { useDispatch } from "react-redux";
+import { deleteContact } from "redux/contactsSlice";
 
 
 
-const List = ({ contacts, onDeleteContact}) => <ul>{contacts.map(({ id, name, number }) => <Item key={id}><p>{name}</p><p>{number}</p>
-<Button onClick = {()=>onDeleteContact(id)}>Delete</Button></Item>)}</ul>
+const List = ({ contacts}) => {
+     const dispatch = useDispatch();
+     const onDeleteContact = (contactId) => {
+    dispatch(deleteContact(contactId));
+  };
+return (<ul>{contacts.map(({ id, name, number }) => <Item key={id}><p>{name}</p><p>{number}</p>
+        <Button onClick={() => onDeleteContact(id)}>Delete</Button></Item>)}</ul>)
+}
 
 export default List;
 
